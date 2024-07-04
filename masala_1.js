541d14087346c8eeaff6b99ad2a6fe882e6294d7
abdumajidov23 <alijonabdumajidov09@gmail.com>
2024-07-04 10:10:02 +0500

    uy_ishi_11.2

--- masala_1.js
+++ masala_1.js
@@ -1,17 +1,30 @@
-function flattenArray(arr) {
-    return arr.reduce((flat, toFlatten) => {
-      return flat.concat(Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten);
-    }, []);
+function flattenarray(array1) {
+  const result = [];
+  for (const i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i])) {
+      result = result.concat(flattenarray(array1[i]));
+    } else {
+      result.push(array1[i]);
+    }
+  }
+  return result;
 }
 
 function difference(arr1, arr2) {
-    const flatArray1 = flattenArray(arr1);
-    const flatArray2 = flattenArray(arr2);
+  const flatArr1 = flattenarray(arr1);
+  const flatArr2 = flattenarray(arr2);
 
-    const unique = new Set([...flatArray1, ...flatArray2]);
+  const combinedArr = flatArr1.concat(flatArr2);
+  const uniqueElements = [];
 
-    return Array.from(unique);
+  for (const i = 0; i < combinedArr.length; i++) {
+    if (uniqueElements.indexOf(combinedArr[i]) === -1) {
+      uniqueElements.push(combinedArr[i]);
+    }
+  }
+
+  return uniqueElements.sort((a, b) => a - b);
 }
 
 console.log(difference([1, 2, 3], [100, 2, 1, 10]));
-console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]], [5, 6]]));
